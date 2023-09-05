66c649dfc5e6964fb042c184ef265b29464ee4f6
liuqt <947363530@qq.com>
2023-09-05 18:00:20 +0800

    add screenshot tool

---
+++ tools/Screenshot/Screenshot/ScreenshotDlg.cpp
@@ -0,0 +1,367 @@
+﻿
+// ScreenshotDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Screenshot.h"
+#include "ScreenshotDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CScreenshotDlg 对话框
+
+
+
+CScreenshotDlg::CScreenshotDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_SCREENSHOT_DIALOG, pParent)
+	, m_StartIndex(0)
+	, m_OutputPath(_T(""))
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CScreenshotDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_START_INDEX, m_StartIndex);
+	DDX_Text(pDX, IDC_EDIT1, m_OutputPath);
+}
+
+BEGIN_MESSAGE_MAP(CScreenshotDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON_CHOOSE_DIRECTORY, &CScreenshotDlg::OnBnClickedButtonChooseDirectory)
+	ON_BN_CLICKED(IDC_BUTTON_OPEN_DIRECTORY, &CScreenshotDlg::OnBnClickedButtonOpenDirectory)
+	ON_BN_CLICKED(IDC_BUTTON3, &CScreenshotDlg::OnBnClickedButton3)
+END_MESSAGE_MAP()
+
+
+// CScreenshotDlg 消息处理程序
+
+BOOL CScreenshotDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CScreenshotDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CScreenshotDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CScreenshotDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+
+BOOL WINAPI SaveDesktopWindow(WCHAR* wPath)
+{
+	BITMAPFILEHEADER bfHeader;
+	BITMAPINFOHEADER biHeader;
+	BITMAPINFO bInfo;
+	HGDIOBJ hTempBitmap;
+	HBITMAP hBitmap;
+	BITMAP bAllDesktops;
+	HDC hDC, hMemDC;
+	LONG lWidth, lHeight;
+	BYTE* bBits = NULL;
+	HANDLE hHeap = GetProcessHeap();
+	DWORD cbBits, dwWritten = 0;
+	HANDLE hFile;
+	INT x = GetSystemMetrics(SM_XVIRTUALSCREEN);
+	INT y = GetSystemMetrics(SM_YVIRTUALSCREEN);
+
+	ZeroMemory(&bfHeader, sizeof(BITMAPFILEHEADER));
+	ZeroMemory(&biHeader, sizeof(BITMAPINFOHEADER));
+	ZeroMemory(&bInfo, sizeof(BITMAPINFO));
+	ZeroMemory(&bAllDesktops, sizeof(BITMAP));
+
+	hDC = GetDC(NULL);
+	hTempBitmap = GetCurrentObject(hDC, OBJ_BITMAP);
+	GetObjectW(hTempBitmap, sizeof(BITMAP), &bAllDesktops);
+
+	lWidth = bAllDesktops.bmWidth;
+	lHeight = bAllDesktops.bmHeight;
+
+	DeleteObject(hTempBitmap);
+
+	bfHeader.bfType = (WORD)('B' | ('M' << 8));
+	bfHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+	biHeader.biSize = sizeof(BITMAPINFOHEADER);
+	biHeader.biBitCount = 24;
+	biHeader.biCompression = BI_RGB;
+	biHeader.biPlanes = 1;
+	biHeader.biWidth = lWidth;
+	biHeader.biHeight = lHeight;
+
+	bInfo.bmiHeader = biHeader;
+
+	cbBits = (((24 * lWidth + 31) & ~31) / 8) * lHeight;
+
+	hMemDC = CreateCompatibleDC(hDC);
+	hBitmap = CreateDIBSection(hDC, &bInfo, DIB_RGB_COLORS, (VOID**)&bBits, NULL, 0);
+	SelectObject(hMemDC, hBitmap);
+	BitBlt(hMemDC, 0, 0, lWidth, lHeight, hDC, x, y, SRCCOPY);
+
+
+	hFile = CreateFileW(wPath, GENERIC_WRITE | GENERIC_READ, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (INVALID_HANDLE_VALUE == hFile)
+	{
+		DeleteDC(hMemDC);
+		ReleaseDC(NULL, hDC);
+		DeleteObject(hBitmap);
+
+		return FALSE;
+	}
+	WriteFile(hFile, &bfHeader, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
+	WriteFile(hFile, &biHeader, sizeof(BITMAPINFOHEADER), &dwWritten, NULL);
+	WriteFile(hFile, bBits, cbBits, &dwWritten, NULL);
+	FlushFileBuffers(hFile);
+	CloseHandle(hFile);
+
+	DeleteDC(hMemDC);
+	ReleaseDC(NULL, hDC);
+	DeleteObject(hBitmap);
+
+	return TRUE;
+}
+
+
+
+void CScreenshotDlg::OnBnClickedButtonChooseDirectory()
+{
+	BROWSEINFOW bi;
+	memset(&bi, 0, sizeof(BROWSEINFOW));
+	WCHAR spDisplayName[MAX_PATH] = { 0 };
+	WCHAR pszPath[MAX_PATH] = { 0 };
+
+	bi.hwndOwner = this->m_hWnd;
+	bi.lpszTitle = L"请选择输出目录";
+	bi.pszDisplayName = spDisplayName;
+	bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_BROWSEINCLUDEFILES | BIF_EDITBOX;
+
+	ITEMIDLIST* idlist = SHBrowseForFolderW(&bi);
+	SHGetPathFromIDListW(idlist, pszPath);
+
+	m_OutputPath = pszPath;
+	UpdateData(FALSE);
+}
+
+
+void CScreenshotDlg::OnBnClickedButton2()
+{
+	
+}
+
+
+void CScreenshotDlg::OnBnClickedButtonOpenDirectory()
+{
+	if (m_OutputPath == L"")
+	{
+		MessageBoxA("please choose source file", "error", 0);
+		return;
+	}
+
+	ShellExecuteA(NULL, NULL, m_OutputPath, // param three: the target path you will open
+		NULL, NULL, SW_SHOW);
+}
+
+
+void CScreenshotDlg::OnBnClickedButton3()
+{
+	if (m_OutputPath == "")
+	{
+		MessageBoxA("请选择输出目录");
+		return;
+	}
+	UpdateData(TRUE);
+
+
+	BITMAPFILEHEADER bfHeader;
+	BITMAPINFOHEADER biHeader;
+	BITMAPINFO bInfo;
+	HGDIOBJ hTempBitmap;
+	HBITMAP hBitmap;
+	BITMAP bAllDesktops;
+	HDC hDC, hMemDC;
+	LONG lWidth, lHeight;
+	BYTE* bBits = NULL;
+	HANDLE hHeap = GetProcessHeap();
+	DWORD cbBits, dwWritten = 0;
+	HANDLE hFile;
+	INT x = GetSystemMetrics(SM_XVIRTUALSCREEN);
+	INT y = GetSystemMetrics(SM_YVIRTUALSCREEN);
+
+	ZeroMemory(&bfHeader, sizeof(BITMAPFILEHEADER));
+	ZeroMemory(&biHeader, sizeof(BITMAPINFOHEADER));
+	ZeroMemory(&bInfo, sizeof(BITMAPINFO));
+	ZeroMemory(&bAllDesktops, sizeof(BITMAP));
+
+	// 查找窗口
+	HWND hwnd = ::FindWindowA(NULL, "地下城与勇士：创新世纪");
+	hDC = ::GetWindowDC(hwnd);
+
+	// 获取宽高
+	//hTempBitmap = GetCurrentObject(hDC, OBJ_BITMAP);
+	//GetObjectA(hTempBitmap, sizeof(BITMAP), &bAllDesktops);
+	//lWidth = bAllDesktops.bmWidth;
+	//lHeight = bAllDesktops.bmHeight;
+	//DeleteObject(hTempBitmap);
+
+	lWidth = 800;
+	lHeight = 600;
+
+	bfHeader.bfType = (WORD)('B' | ('M' << 8));
+	bfHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+	biHeader.biSize = sizeof(BITMAPINFOHEADER);
+	biHeader.biBitCount = 24;
+	biHeader.biCompression = BI_RGB;
+	biHeader.biPlanes = 1;
+	biHeader.biWidth = lWidth;
+	biHeader.biHeight = lHeight;
+
+	bInfo.bmiHeader = biHeader;
+	cbBits = (((24 * lWidth + 31) & ~31) / 8) * lHeight;
+
+
+	//RECT rect;
+	//::GetClientRect(hwnd, &rect);
+	//HBITMAP hBitmap = CreateCompatibleBitmap(hdc, rect.right - rect.left, rect.bottom - rect.top);
+
+	hMemDC = CreateCompatibleDC(hDC);
+	hBitmap = CreateDIBSection(hDC, &bInfo, DIB_RGB_COLORS, (VOID**)&bBits, NULL, 0);
+	SelectObject(hMemDC, hBitmap);
+	BitBlt(hMemDC, 0, 0, lWidth, lHeight, hDC, x, y, SRCCOPY);
+
+	// 写入到文件
+	CString outfilename;
+	outfilename.Format("%s\\%05d.jpg", m_OutputPath, m_StartIndex++);
+	hFile = CreateFileA(outfilename, GENERIC_WRITE | GENERIC_READ, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (INVALID_HANDLE_VALUE == hFile)
+	{
+		return;
+	}
+	WriteFile(hFile, &bfHeader, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
+	WriteFile(hFile, &biHeader, sizeof(BITMAPINFOHEADER), &dwWritten, NULL);
+	WriteFile(hFile, bBits, cbBits, &dwWritten, NULL);
+	FlushFileBuffers(hFile);
+	CloseHandle(hFile);
+
+
+	// 清理DC
+	DeleteObject(hBitmap);
+	DeleteDC(hMemDC);
+	::ReleaseDC(hwnd, hDC);
+
+	UpdateData(FALSE);
+
+}
